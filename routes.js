220308921412617a954255b47fca127f253b4b27
GDC-Reis <gustavodonisete11@gmail.com>
2025-03-04 12:29:22 -0300

    :construction: Commit realizado com adição do recebimento do parâmetro na URL

--- routes.js
+++ routes.js
@@ -6,4 +6,9 @@ module.exports = [
     method: 'GET',
     handler: userContoller.listUsers,
   },
+  {
+    endpoint: '/users/:id',
+    method: 'GET',
+    handler: userContoller.getUserById,
+  },
 ]
--- src/index.js
+++ src/index.js
@@ -2,18 +2,31 @@ const http = require('http');
 const { URL } = require('url');
 
 const routs = require('../routes');
+const path = require('path');
 
 const server = http.createServer((request, response) => {
   const parsedUrl = new URL(`http://localhost:3000${request.url}`);
+  
+  let { pathname } = parsedUrl;
+  let id = null;
+
+  const splitEndPoint = pathname.split('/').filter(Boolean);
+  
+  if(splitEndPoint.length > 1){
+    pathname = `/${splitEndPoint[0]}/:id`
+    id = splitEndPoint[1];
+  }
 
   console.log(`Request Method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
 
   const route = routs.find((routeObj) => (
-    routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
+    routeObj.endpoint === pathname && routeObj.method === request.method
   ));
 
   if(route) {
-    request.query = Object.fromEntries(parsedUrl.searchParams);
+    request.query = parsedUrl.query;
+    request.params = { id };
+
     route.handler(request, response);
   }else{
     response.writeHead(404, {'Content-Type': 'text/html'});
